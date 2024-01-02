e201c1c5a716b21b57e1dfd5482d730c3cfdb3ca
erikakr <erika.kriksciunaite@nordsec.com>
2024-01-02 22:27:06 +0200

    functions for Part 2 with printing to the terminal

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <cctype>
 #include <map>
 
 // Function to clean and process a word
@@ -26,5 +28,37 @@ void writeWordCountToFile(const std::map<std::string, int> &wordCount, const std
         std::cout << "Output written to file '" << filename << "'" << std::endl;
     } else {
         std::cerr << "Unable to open output file!" << std::endl;
+    }
+}
+
+void processLine(const std::string &line, int lineNumber, std::map<std::string, std::vector<std::pair<int, int>>> &wordLocations) {
+    std::istringstream iss(line);
+    std::string word;
+    while (iss >> word) {
+        std::string cleanedWord = cleanWord(word);
+        if (!cleanedWord.empty()) {
+            bool wordFound = false;
+            for (auto &pair : wordLocations[cleanedWord]) {
+                if (pair.first == lineNumber) {
+                    pair.second++;
+                    wordFound = true;
+                    break;
+                }
+            }
+            if (!wordFound) {
+                wordLocations[cleanedWord].push_back(std::make_pair(lineNumber, 1));
+            }
+        }
+    }
+}
+
+void printCrossReference(const std::map<std::string, std::vector<std::pair<int, int>>> &wordLocations) {
+    for (const auto &word : wordLocations) {
+        std::cout << "Word: " << word.first << std::endl;
+        std::cout << "  Appears in lines: ";
+        for (const auto &pair : word.second) {
+            std::cout << "Line " << pair.first << " (" << pair.second << " times) ";
+        }
+        std::cout << std::endl;
     }
 }
--- functions.h
+++ functions.h
@@ -6,5 +6,6 @@
 
 std::string cleanWord(const std::string &word);
 void writeWordCountToFile(const std::map<std::string, int> &wordCount, const std::string &filename);
-
+void processLine(const std::string &line, int lineNumber, std::map<std::string, std::vector<std::pair<int, int>>> &wordLocations);
+void printCrossReference(const std::map<std::string, std::vector<std::pair<int, int>>> &wordLocations);
 #endif // FUNCTIONS_H_INCLUDED
